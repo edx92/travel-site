83a63c60dc83d0a54147ea6d38001bc20536daf8
Ed <ed@edx92-design.co.uk>
2021-01-27 18:12:44 +0000

    Changes to dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,20 @@ module.exports = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
+    // setup for dev server with auto refresh for html changes and hot modules for css & js
+    // also adding support for network connected device testing
+    devServer: {
+        before: function (app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
     mode: 'development',
-    watch: true,
+    // Below required if not using webpack-dev-server
+    // watch: true,
     module: {
         rules: [
             {
