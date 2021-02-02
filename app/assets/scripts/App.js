52c3156be52a4c828e395d14890cc3f8fc25220c
Ed <ed@edx92-design.co.uk>
2021-02-02 19:55:36 +0000

    Added modal functionality including esc key exit feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,11 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/revealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 let mobileMenu = new MobileMenu();
 let stickyHeader = new StickyHeader();
+new Modal();
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
