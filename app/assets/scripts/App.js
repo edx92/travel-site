c2336e0d5caea3b0767a695183e99e2003ac7b5e
Ed <ed@edx92-design.co.uk>
2021-02-03 08:29:48 +0000

    Completed the modal overlay. Seperated the code for this entire section and changed its trigger to run from the main app.js and only download the modal.js file when required

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,14 +2,29 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/revealOnScroll';
 import StickyHeader from './modules/StickyHeader';
-import Modal from './modules/Modal';
+
 
 let mobileMenu = new MobileMenu();
 let stickyHeader = new StickyHeader();
-new Modal();
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+
+let modal;
+
+document.querySelectorAll('.open-modal').forEach(el => {
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        if (typeof modal == 'undefined') {
+            import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openTheModal(), 20);
+            }).catch(() => console.log("There was a problem loading Modal"))
+        } else {
+            modal.openTheModal();
+        }
+    });
+});
 
 if (module.hot) {
     module.hot.accept();
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -1,46 +1,49 @@
+// commented out code is due to these functions being controlled in our seperated code. 
+// Loading the modal Class only when required and handling some of its basic functions.
+
 class Modal {
-    constructor() {
-        this.injectHTML();
-        this.openModalButtons = document.querySelectorAll('.open-modal');
-        this.modal = document.querySelector('.modal');
-        this.closeIcon = document.querySelector('.modal__close');
+  constructor() {
+    this.injectHTML();
+    // this.openModalButtons = document.querySelectorAll('.open-modal');
+    this.modal = document.querySelector('.modal');
+    this.closeIcon = document.querySelector('.modal__close');
 
-        this.events();
+    this.events();
 
+  }
+
+  // events
+  events() {
+    // listen for open click
+    // this.openModalButtons.forEach(el => el.addEventListener('click', e => this.openTheModal(e)));
+
+    // listen for close click
+    this.closeIcon.addEventListener('click', el => this.closeTheModal());
+
+    // pushes any key
+    document.addEventListener('keyup', e => this.keyPressHandler(e));
+
+  }
+
+
+  // Methods
+  openTheModal(e) {
+    // e.preventDefault();
+    this.modal.classList.add('modal--is-visible');
+  }
+
+  closeTheModal() {
+    this.modal.classList.remove('modal--is-visible');
+  }
+
+  keyPressHandler(e) {
+    if (e.keyCode == 27) {
+      this.closeTheModal();
     }
+  }
 
-    // events
-    events() {
-        // listen for open click
-        this.openModalButtons.forEach(el => el.addEventListener('click', e => this.openTheModal(e)));
-
-        // listen for close click
-        this.closeIcon.addEventListener('click', el => this.closeTheModal());
-
-        // pushes any key
-        document.addEventListener('keyup', e => this.keyPressHandler(e));
-
-    }
-
-
-    // Methods
-    openTheModal(e) {
-        e.preventDefault();
-        this.modal.classList.add('modal--is-visible');
-    }
-
-    closeTheModal() {
-        this.modal.classList.remove('modal--is-visible');
-    }
-
-    keyPressHandler(e) {
-        if (e.keyCode == 27) {
-            this.closeTheModal();
-        }
-    }
-
-    injectHTML() {
-        document.body.insertAdjacentHTML('beforeend', `
+  injectHTML() {
+    document.body.insertAdjacentHTML('beforeend', `
         <div class="modal">
         <div class="modal__inner">
           <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
@@ -58,7 +61,7 @@ class Modal {
         <div class="modal__close">X</div>
       </div>
       `);
-    }
+  }
 }
 
 
